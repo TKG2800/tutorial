d0d3e36d1856eb86eaacf8592735c4e32c3261bd
TKG2800 <takumi1997iwamatsu@yahoo.co.jp>
2025-05-26 22:10:16 +0900

    [add] index.html

---
+++ sim/monkey.ts
@@ -0,0 +1,21 @@
+type obj = {
+ x: number;
+ y: number;
+};
+const g = 9.8;
+const acceleration = (a: number,t: number) => {
+  y = at;
+  return y;
+};
+fire: number;
+angle: number;
+b: obj = {
+   y: fire * Math.cos(angle) * t - 1 / 2 * acceleration(g,t) * t;
+   x: fire * Math.sin(angle);
+};
+
+monkey: obj = {
+   y: 1 / 2 * acceleration(g,t) * t;
+   x: t;
+};
+
